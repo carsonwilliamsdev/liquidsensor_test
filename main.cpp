faadd2d2a64fd5cafaf2e83f71b2b84c5b435f77
Carson Williams <sxhatrix@gmail.com>
2013-07-26 02:40:37 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,12 +13,12 @@ union RawFloat
 
 int main()
 {
-    /* array to store register results */
+    	/* array to store register results */
 	uint16_t tab_reg[2*sizeof(uint16_t)];
 	memset(tab_reg, 0, 2*sizeof(uint16_t));
 
 	/* open connection to the sensor */
-    modbus_t *mb;
+    	modbus_t *mb;
 	mb = modbus_new_tcp("10.0.0.2", 1502);
 	modbus_connect(mb);
 
@@ -26,18 +26,18 @@ int main()
 	if(modbus_read_input_registers(mb, 0x0004, 2, tab_reg) == -1)
 		printf("Reading Temperature Failed\n");
 
-    /* data is IEEE-754 float, but in network byte order so swap for little endian. */
+    	/* data is IEEE-754 float, but in network byte order so swap for little endian. */
 	tab_reg[0] = ((tab_reg[0] << 8) + (tab_reg[0] >> 8));
 	tab_reg[1] = ((tab_reg[1] << 8) + (tab_reg[1] >> 8));
 
-    /* register 4 is the least significant word, and register 5 is the most significant word,
-       so add the translated values together in reverse order to create a float */
+    	/* register 4 is the least significant word, and register 5 is the most significant word,
+       	so add the translated values together in reverse order to create a float */
 	RawFloat temp;
 	temp.raw = ((tab_reg[1] << 16) + tab_reg[0]);
 
 	printf("Temperature: %f C\n", temp.f);
 
-    /* Close Sensor Connection */
+    	/* Close Sensor Connection */
 	modbus_close(mb);
 	modbus_free(mb);
 	return 0;
